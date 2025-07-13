0541b551e7fc81abd20d803cf9afbda5dac282bc
TALHAdevelops <160895671+TALHAdevelops@users.noreply.github.com>
2025-07-13 14:55:46 +0500

    new

---
+++ chap 05/script.js
@@ -0,0 +1,51 @@
+//Question No 1
+
+var num1 = 10
+var num2 = 20;
+
+var sum = num1 + num2;
+document.write("Sum of " + num1 + " and " + num2 + " is " + sum + "<br>");
+
+//Question No 2
+
+var sub = num2 - num1;
+document.write("Subtraction of " + num2 + " and " + num1 + " is " + sub + "<br>");
+
+var mul = num1 * num2;
+document.write("Multiplication of " + num1 + " and " + num2 + " is " + mul + "<br>");
+
+var div = num2 / num1;
+document.write("Division of " + num2 + " and " + num1 + " is " + div + "<br>");
+
+var mod = num2 % num1;
+document.write("Modulus of " + num2 + " and " + num1 + " is " + mod + "<br>");
+
+
+//Question No 3
+
+//a
+var variable;
+//b
+document.write("Value after variable declaration is: " + variable + "<br>");
+//c
+variable = 5;
+//d
+document.write("Initial value: " + variable + "<br>");
+//e
+variable++;
+//f
+document.write("Value after increment is: " + variable + "<br>");
+//g
+variable += 7;
+//h
+document.write("Value after addition is: " + variable + "<br>");
+//i
+variable--;
+//j
+document.write("Value after decrement is: " + variable + "<br>");
+//k
+variable %= 3;
+//l
+document.write("The remainder is: " + variable + "<br>");
+
+
